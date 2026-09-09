1088f6e06662d9a9ad8fa1761789c71ac79c9f87
Hajar Mrifag <hajarmrifag2@gmail.com>
2026-09-09 21:33:21 +0800

    Wire SQL Server database context into API

--- src/Claims.API/Program.cs
+++ src/Claims.API/Program.cs
@@ -1,14 +1,18 @@
+using Claims.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ClaimsDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("ClaimsDatabase")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
